7099f881dc5b65c458494f1274db411797ffea72
Pappy <65431875+Pappy-Hub@users.noreply.github.com>
2022-09-08 16:49:57 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database-1.crxfkdowmtfi.us-east-1.rds.amazonaws.com 
+export POSTGRES_USERNAME=####
+export POSTGRES_PASSWORD=####
+export POSTGRES_HOST=####
 export POSTGRES_DB=postgres
-export AWS_BUCKET=pappy-micro
-export AWS_REGION=us-east-1
+export AWS_BUCKET=####
+export AWS_REGION=####
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://localhost:8080
+export URL=http://localhost:####
