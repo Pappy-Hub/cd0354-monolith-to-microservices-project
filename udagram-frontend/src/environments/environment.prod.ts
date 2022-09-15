88c45cb0741168f56cc0a4801652510a0b6cc38c
Pappy-Hub <boyshokan@gmail.com>
2022-09-15 03:35:16 +0100

    docker build

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aef5cc838983c4985bf713d3e00b2742-1541187941.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a1a940c7a60e64e60bb4deb9f9cd97e4-1650545088.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
